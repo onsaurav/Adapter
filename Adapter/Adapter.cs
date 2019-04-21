8c45939f822d181a9c260dda8ca6aaa79cc3fd73
Kartik Chandra Biswas <onsaurav@hotmail.com>
2019-04-21 20:47:33 +0600

    Initial commit

---
+++ Adapter/Adapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Adapter:Target
+    {
+       private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            _adaptee.SpecificRequest();
+        }
+        
+    }
+}
